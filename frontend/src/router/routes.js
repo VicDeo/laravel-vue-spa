c74fb17ec8dee83718f35963dcaea443c338eabe
VicDeo <victor.dubiniuk@gmail.com>
2024-10-26 19:18:01 +0200

    Use env variables for api endpoints, add auth api

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -15,7 +15,7 @@ const routes = [
         path: "/tasks",
         component: TasksPage,
         name: 'tasks',
-        meta: {auth: true}
+        // meta: {auth: true}
     },    {
         path: "/summary",
         component: SummaryPage,
