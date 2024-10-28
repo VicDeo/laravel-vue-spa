8d221ac3f3096391f8870343798294e88e82e2e7
VicDeo <victor.dubiniuk@gmail.com>
2024-10-28 19:22:19 +0100

    Implement Register flow

--- frontend/src/pages/LoginPage.vue
+++ frontend/src/pages/LoginPage.vue
@@ -6,7 +6,6 @@
                 <strong>List</strong>
             </h1>
             <h2 class="h3 mb-4 fw-normal">Please sign in</h2>
-            {{ errors }}
             <div class="form-floating mb-2">
                 <input
                     type="email"
